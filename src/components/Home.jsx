a946515969316d4209d0a688c4300661109659da
Guilherme <guilherme@sunsetsolutions.com.br>
2022-11-03 13:17:03 -0300

    Working on contact form

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState, Suspense } from "react";
-import { Box, Flex, Text, Heading, Image, Divider, Container, Button, HStack, Spinner } from '@chakra-ui/react';
+import { Box, Flex, Text, Heading, Image, Divider, Container, Button, HStack, Spinner, Tooltip, Input } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 
@@ -7,7 +7,7 @@ const testimonials = [
   {
     id: 'amy',
     image: '/assets/amy.png',
-    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.",
+    text: "Eu adquiri uma propriedade recentemente, o residencial Edinburgh é maravilhoso! Meus filhos amam!",
     size: 24,
     top: 0,
     right: 'unset',
@@ -16,7 +16,7 @@ const testimonials = [
   {
     id: 'andrea',
     image: '/assets/andrea.png',
-    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.",
+    text: "O que eu mais gosto no residencial Edinburgh é a segurança e a tranquilidade. Sem falar da localização que também é muito boa!",
     size: 20,
     top: 52,
     right: 'unset',
@@ -25,7 +25,7 @@ const testimonials = [
   {
     id: 'emily',
     image: '/assets/emily.png',
-    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.",
+    text: "Eu nunca morei num lugar como esse. É simplesmente maravilhoso!",
     size: 40,
     top: 96,
     right: 'unset',
@@ -34,7 +34,7 @@ const testimonials = [
   {
     id: 'lawrence',
     image: '/assets/lawrence.png',
-    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.",
+    text: "Quando eu pensei em adquirir uma propriedade no residencial Edinburgh eu estava em dúvida, mas os corretores da Lust me mostraram diversos motivos para adquiri-lo. Qualidade do atendimento é impecável!",
     size: 20,
     top: 0,
     left: 'unset',
@@ -43,7 +43,7 @@ const testimonials = [
   {
     id: 'leticia',
     image: '/assets/leticia.png',
-    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.",
+    text: "Estou amando morar no residencial Edinburgh!",
     size: 24,
     top: 52,
     left: 'unset',
@@ -52,7 +52,7 @@ const testimonials = [
   {
     id: 'matt',
     image: '/assets/matt.png',
-    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.",
+    text: "Se eu pudesse traria toda a minha família para morar aqui, é maravilhoso!",
     size: 32,
     top: 96,
     left: 'unset',
@@ -126,7 +126,7 @@ const Home = () => {
           <Flex as={motion.div} initial={{ opacity: 0, x: 100 }} whileInView={{ opacity: 1, x: 0 }} transition='.6s linear' h='80%' w='55%' justifyContent='center' align-items='center' position='relative'>
             <Box bg='#000' h='100%' w={{ '2xl': 10 }} position='absolute' top={0} right={10} zIndex='9'/>
             <Suspense fallback={<Spinner />}> 
-              <Image src='/assets/house1.jpg' alt="house1" display='block' h='100%' w='auto' transform='translateX(100px)' objectFit='cover' />
+              <Image src='/assets/house1.webp' alt="house1" display='block' h='100%' w='auto' transform='translateX(100px)' objectFit='cover' />
             </Suspense>
           </Flex>
         </Flex>
@@ -136,7 +136,7 @@ const Home = () => {
         <Flex w='100%' h='100%'>
           <Box as={motion.div} initial={{ opacity: 0, y: 60 }} whileInView={{ opacity: 1, y: 0 }} transition='1s linear' w='40%' mr={28}>
             <Suspense fallback={<Spinner />}> 
-              <Image src='/assets/house2.jpg' alt="house2" display='block' h={{ '2xl': '700px' }} w='auto' objectFit='cover' />
+              <Image src='/assets/house2.webp' alt="house2" display='block' h={{ '2xl': '700px' }} w='auto' objectFit='cover' />
             </Suspense>
           </Box>
           <Box w='60%' as={motion.div} initial={{ opacity: 0, y: 60 }} whileInView={{ opacity: 1, y: 0 }} transition='1s linear'>
@@ -183,7 +183,7 @@ const Home = () => {
           <Flex w='100%' h='100%' justifyContent='space-evenly' alignItems='flex-start' gap={6} py={5}>
             <Box position='relative' h='85%' w='50%'>
               <Suspense fallback={<Spinner />}> 
-                <Image src='/assets/house3.jpg' h='100%' w='100%' alt='house3' objectFit='cover' />
+                <Image src='/assets/house3.webp' h='100%' w='100%' alt='house3' objectFit='cover' />
               </Suspense>
               <Box w={{ '2xl': '70%' }} h={{ '2xl': '100px' }} p={4} bg='#fff' position='absolute' bottom={6} left='0' right='0' mx='auto' borderRadius='15px' boxShadow='lg'>
                 <Text fontSize={20} fontWeight='700' color='#000'>Macdowell's House</Text>
@@ -196,7 +196,7 @@ const Home = () => {
             <Flex flexDir='column' justifyContent='center' alignItems='center' h='85%' w='50%' gap={6}>
               <Box position='relative' h='48%' w='100%'>
                 <Suspense fallback={<Spinner />}> 
-                  <Image src='/assets/house4.jpg' h='100%' w='100%' alt='house4' objectFit='cover' />
+                  <Image src='/assets/house4.webp' h='100%' w='100%' alt='house4' objectFit='cover' />
                 </Suspense>
                 <Box w={{ '2xl': '70%' }} h={{ '2xl': '100px' }} p={4} bg='#fff' position='absolute' bottom={6} left='0' right='0' mx='auto' borderRadius='15px' boxShadow='lg'>
                   <Text fontSize={20} fontWeight='700' color='#000'>Thompson's House</Text>
@@ -208,7 +208,7 @@ const Home = () => {
               </Box>
               <Box position='relative' h='48%' w='100%'>
                 <Suspense fallback={<Spinner />}> 
-                  <Image src='/assets/house5.jpg' h='100%' w='100%' alt='house5' objectFit='cover' />
+                  <Image src='/assets/house5.webp' h='100%' w='100%' alt='house5' objectFit='cover' />
                 </Suspense>
                 <Box w={{ '2xl': '70%' }} h={{ '2xl': '100px' }} p={4} bg='#fff' position='absolute' bottom={6} left='0' right='0' mx='auto' borderRadius='15px' boxShadow='lg'>
                   <Text fontSize={20} fontWeight='700' color='#000'>Johnson's House</Text>
@@ -227,28 +227,51 @@ const Home = () => {
         <Box as={motion.div} initial={{ opacity: 0, y: 60 }} whileInView={{ opacity: 1, y: 0 }} transition='1s linear'>
           <Heading as='h4' textAlign='center' fontSize={70} color='#fff' mb={14}>O que nossos clientes dizem</Heading>
         </Box>
-        <Container id="testimonials" display='flex' justifyContent='center' alignItems='space-between' h={{ '2xl': '60%' }} maxW='90%' position='relative'>
-          {
-              testimonials.map((testimonial, index) => {
-                  return (testimonial.id !== activeTestimonial) ? <Box key={index} data-id={testimonial.id} position='absolute' top={testimonial.top} left={testimonial.left} right={testimonial.right} h={testimonial.size} w={testimonial.size} borderRadius='100%' overflow='hidden' cursor='pointer' onClick={handleTestimonial}>
-                    <Image h='100%' w='100%' src={testimonial.image} alt={testimonial.id} objectFit='contain' />
-                  </Box> : ''
-              })
-          }
-          <Box className='activeTestimonial' position='absolute' insetY={0} insetX={0} m='auto' h={{ '2xl': '600px' }} maxW={{ '2xl': '800px' }}>
-            <Flex w='100%' h='100%' flexDir='column' justifyContent='center' alignItems='center'>
-              <Box data-id={testimonials[activeTestimonialIndex].id} position='relative' h={{ '2xl': 52 }} w={{ '2xl': 52 }} overflow='hidden' borderRadius='100%' mb={3}>
-                <Image h='100%' w='100%' src={testimonials[activeTestimonialIndex].image} alt='Sonya' objectFit='contain' objectPosition='bottom' />
-              </Box>
-              <Text fontSize={18} textTransform='capitalize' color='#fff' mb={8} textAlign='center'>
-                {testimonials[activeTestimonialIndex].id}
-              </Text>
-              <Text fontSize={20} color='#fff' maxW='80%' textAlign='center'>
-                {testimonials[activeTestimonialIndex].text}
-              </Text>
-            </Flex>
+        <Box as={motion.div} w='100%' h={{ '2xl': '60%' }} initial={{ opacity: 0, y: 60 }} whileInView={{ opacity: 1, y: 0 }} transition='1s linear'>
+          <Container id="testimonials" display='flex' justifyContent='center' alignItems='space-between' h='100%' maxW={{ '2xl': '70%' }} position='relative'>
+              {
+                  testimonials.map((testimonial, index) => {
+                      return (testimonial.id !== activeTestimonial) ? <Box key={index} data-id={testimonial.id} position='absolute' top={testimonial.top} left={testimonial.left} right={testimonial.right} h={testimonial.size} w={testimonial.size} borderRadius='100%' overflow='hidden' cursor='pointer' onClick={handleTestimonial}>
+                        <Tooltip label='Clique para ver a avaliação' placement='top' bg='gray.50' color='gray.900'>
+                          <Image h='100%' w='100%' src={testimonial.image} alt={testimonial.id} objectFit='contain' />
+                        </Tooltip>
+                      </Box> : ''
+                  })
+              }
+              <Box className='activeTestimonial' position='absolute' insetY={0} insetX={0} m='auto' h={{ '2xl': '600px' }} maxW={{ '2xl': '800px' }}>
+                <Flex w='100%' h='100%' flexDir='column' justifyContent='center' alignItems='center'>
+                  <Box data-id={testimonials[activeTestimonialIndex].id} position='relative' h={{ '2xl': 52 }} w={{ '2xl': 52 }} overflow='hidden' borderRadius='100%' mb={3}>
+                    <Image h='100%' w='100%' src={testimonials[activeTestimonialIndex].image} alt='Sonya' objectFit='contain' objectPosition='bottom' />
+                  </Box>
+                  <Text fontSize={18} textTransform='capitalize' color='#fff' mb={8} textAlign='center'>
+                    {testimonials[activeTestimonialIndex].id}
+                  </Text>
+                  <Text fontSize={20} color='#fff' maxW='80%' textAlign='center'>
+                    {testimonials[activeTestimonialIndex].text}
+                  </Text>
+                </Flex>
+              </Box>  
+          </Container>
+        </Box>
+      </Box>
+
+      <Box id="contactSection" h='100%' w='100%'>
+        <Flex justifyContent='space-between' alignItems='flex-start' mb={12}>
+          <Heading w='40%' as='h4' textAlign='left' fontSize={60} color='#fff' mb={14}>Interessado em alguma propriedade?</Heading>
+          <Text w='40%' fontSize={20} color='gray.300' textAlign='left'>Preencha o formulário para agendar uma visita ao residencial Edinburgh. Caso você tenha alguma dúvida, não hesite em nos enviar uma mensagem!</Text>
+        </Flex>
+        <Flex id="contactForm" justifyContent='space-between' alignItens='flex-start'>
+          <Box w='40%'>
+            <Input h={14} borderRadius='0' bg='#fff' placeholder="Digite seu nome completo" fontSize={20} mb={10} />
+            <Input h={14} borderRadius='0' bg='#fff' placeholder="Digite seu apelido" fontSize={20} mb={10} />
+            <Input h={14} borderRadius='0' bg='#fff' placeholder="Digite seu telefone celular" fontSize={20} />
           </Box>
-        </Container>
+          <Box w='40%'>
+            <Input h={14} borderRadius='0' bg='#fff' placeholder="Digite seu email" fontSize={20} mb={10} />
+            <Input h={14} borderRadius='0' bg='#fff' placeholder="Digite seu gênero" fontSize={20} mb={10} />
+            <Input h={14} borderRadius='0' bg='#fff' placeholder="Digite sua data de nascimento" fontSize={20} />
+          </Box>
+        </Flex>
       </Box>
     </Container>
   )
