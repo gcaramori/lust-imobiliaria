fdd78889529d3df6936d4f0382214e9a1f9ad66f
Guilherme <guilherme@sunsetsolutions.com.br>
2022-11-11 13:44:36 -0300

    Working on Schedule component

--- src/components/includes/Schedule.jsx
+++ src/components/includes/Schedule.jsx
@@ -3,52 +3,38 @@ import { Flex, Button, Text } from '@chakra-ui/react';
 
 const content = (
     <Flex py={4}>
-      <Text p={1}>BLA</Text>
+      
     </Flex>
   );
   
   const steps = [
-    { label: 'Step 1', content },
-    { label: 'Step 2', content },
-    { label: 'Step 3', content },
+    { label: 'Escolha uma data', content },
+    { label: 'Preencha o formulário', content }
   ];
-  
+
   const Schedule = () => {
-    const { nextStep, prevStep, setStep, reset, activeStep } = useSteps({
+    const { nextStep, activeStep } = useSteps({
       initialStep: 0,
     });
+
+    const handleSchedule = () => {
+      nextStep();
+    }
   
     return (
       <Flex flexDir="column" width="100%">
         <Steps activeStep={activeStep}>
           {steps.map(({ label, content }) => (
-            <Step label={label} key={label}>
+            <Step fontFamily='Poppins, sans-serif' label={label} key={label}>
               {content}
             </Step>
           ))}
         </Steps>
-        {activeStep === steps.length ? (
-          <Flex p={4}>
-            <Button mx="auto" size="sm" onClick={reset}>
-              Reset
+        <Flex width="100%" justify="flex-end">
+            <Button size="sm" onClick={handleSchedule}>
+              {activeStep === steps.length - 1 ? 'Pronto!' : 'Próximo'}
             </Button>
           </Flex>
-        ) : (
-          <Flex width="100%" justify="flex-end">
-            <Button
-              isDisabled={activeStep === 0}
-              mr={4}
-              onClick={prevStep}
-              size="sm"
-              variant="ghost"
-            >
-              Prev
-            </Button>
-            <Button size="sm" onClick={nextStep}>
-              {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-            </Button>
-          </Flex>
-        )}
       </Flex>
     );
 };
--- src/components/includes/ScheduleButton.jsx
+++ src/components/includes/ScheduleButton.jsx
@@ -7,7 +7,7 @@ const ScheduleButton = ({ buttonStyle }) => {
     
     const ButtonElement = ({ buttonStyle }) => {
         if(buttonStyle === 'headerButton') {
-            return <Button onClick={handleSchedule} as='a' w='95%' mx='auto' bg='gray.50' color='#000' mt={{ base: 4, md: 8 }} borderRadius={0} boxShadow='lg' _hover={{ bg: 'gray.200' }} cursor='pointer'>AGENDE UMA VISITA</Button>
+            return <Button onClick={handleSchedule} as='a' w='95%' mx='auto' bg='gray.50' color='#000' fontSize={{ base: 12, lg: 16 }} mt={{ base: 4, md: 8 }} borderRadius={0} boxShadow='lg' _hover={{ bg: 'gray.200' }} cursor='pointer'>AGENDE UMA VISITA</Button>
         }
         else if(buttonStyle === 'navButton') {
             return <Button onClick={handleSchedule} bg='gray.50' color='#000' borderRadius={0} py={8} ml={4} _hover={{ bg: ' gray.200' }}>AGENDE UMA VISITA</Button>
@@ -27,7 +27,7 @@ const ScheduleButton = ({ buttonStyle }) => {
         <>
             <ButtonElement buttonStyle={buttonStyle} />
 
-            <Modal isOpen={isOpen} onClose={onClose}>
+            <Modal isOpen={isOpen} onClose={onClose} size='6xl'>
                 <ModalOverlay />
                 <ModalContent>
                     <ModalHeader>Agendar uma visita</ModalHeader>
